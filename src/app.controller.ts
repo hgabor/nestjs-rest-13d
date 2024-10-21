828775beb04f8a8148ee569ced730cf6dd437406
Petrik Diak <diak@petrik.hu>
2024-10-21 14:34:17 +0200

    Resources

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Delete, Get, InternalServerErrorException, NotFoundException, Param, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateProductDto } from './createproduct.dto';
 
 @Controller()
 export class AppController {
@@ -11,5 +12,48 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  #products = [
+    {
+      name: 'Bucket',
+      price: 3500,
+    },
+    {
+      name: 'REST API for dummies',
+      price: 7850,
+    },
+    {
+      name: 'Tablet',
+      price: 45000,
+    }
+  ];
+
+  @Get('products')
+  listProducts() {
+    return this.#products;
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    const idNum = Number(id);
+    if (idNum >= this.#products.length || idNum < 0) {
+      throw new NotFoundException("No product with this ID")
+    }
+    return this.#products[Number(id)];
+  }
+
+  @Delete('products/:id')
+  deleteProduct(@Param('id') id: string) {
+    const idNum = Number(id);
+    if (idNum >= this.#products.length || idNum < 0) {
+      throw new NotFoundException("No product with this ID")
+    }
+    this.#products.splice(idNum, 1);
+  }
+
+  @Post('products')
+  newProduct(@Body() createProductDto: CreateProductDto) {
+    this.#products.push(createProductDto);
   }
 }
